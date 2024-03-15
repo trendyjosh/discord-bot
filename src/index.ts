aa3c71114bf638cc19db97501f09949b12787e74
Josh <60097494+trendyjosh@users.noreply.github.com>
2024-03-15 20:43:35 +0000

    Remove redundant axios config

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,9 @@ import { SapphireClient, container } from "@sapphire/framework";
 import { GatewayIntentBits } from "discord.js";
 import config from "./config.json";
 import { Controller } from "./controllers/Controller";
-import axios from "axios";
 
 // Create the controller object
 container.controller = new Controller();
-
-// Set axios defaults
-axios.defaults.headers.common["Authorization"] = config.clickupToken;
-axios.defaults.baseURL = config.clickupEndpoint;
 
 // Initiate the Sapphire client
 const client = new SapphireClient({
